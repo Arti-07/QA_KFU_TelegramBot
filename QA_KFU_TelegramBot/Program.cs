c879ef3df1c56fb33043594094c298a7649d26a1
Artur Gin <frank.tr@mail.ru>
2021-05-14 13:09:59 +0300

    Add project files.

---
+++ QA_KFU_TelegramBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QA_KFU_TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
